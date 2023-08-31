d907b49911229584b3adb60ab62056a1a584956e
Hares-788-Cyber <142331272+Hares-788-Cyber@users.noreply.github.com>
2023-08-31 15:25:27 +0600

    Add files via upload

---
+++ mpl.py
@@ -0,0 +1,90 @@
+import os
+
+os.system('clear')
+
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+
+def Zeek():
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+    os.system('clear')
+
+
+print("""
+
+
+\033[1;93m _______   _______ ___________     ___________  _____ 
+\033[1;92m/  __ \ \ / / ___ \  ___| ___ \   |___  /  _  ||  _  |
+\033[1;93m| /  \/\ V /| |_/ / |__ | |_/ /_____ / / \ V /  \ V / 
+\033[1;92m| |     \ / | ___ \  __||    /______/ /  / _ \  / _ \ 
+\033[1;93m| \__/\ | | | |_/ / |___| |\ \    ./ /  | |_| || |_| |
+\033[1;92m \____/ \_/ \____/\____/\_| \_|   \_/   \_____/\_____/
+                                                      
+                                                      
+	       \033[1;94m["WELCOME TO OUR world ðŸŒ"]
+                                                           
+\033[1;31m[\033[1;32m+\033[1;31m]\033[1;93m═════════════════════════════════════════════════\033[1;31m[\033[1;32m+\033[1;31m]
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m CREATED BY\33[0;m   : \033[1;96mTanvir Hossen 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m FACEBOK      : \033[1;34mTanvir Hossen 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m GITHUB       : \033[1;35mHares-788-Cyber      
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TOOL STATUS  : \033[1;36mRendom   
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TEAM         : \033[1;35mCyber-788                 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TOOL VIRSION : \033[1;36m1.0                   
+\033[1;31m[\033[1;32m+\033[1;31m]\033[1;93m═════════════════════════════════════════════════\033[1;31m[\033[1;32m+\033[1;31m]""")
+print("""
+  \033[1;91m[\033[1;97m1\033[1;91m]\033[1;32m RANDOM ID CLONE \033[1;93m
+  \033[1;91m[\033[1;97m2\033[1;91m]\033[1;32m AUTO SHARE \033[1;91m
+  \033[1;91m[\033[1;97m3\033[1;91m]\033[1;32m LOVE \033[1;93m
+  """)
+
+pil = input("  \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system('rm -rf Mymen & git clone https://github.com/Hares-788-Cyber/Mymen')
+
+    os.system('cd Mymen && python hp.py my')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+elif pil in ["02", "2"]:
+
+    os.system('rm -rf Mymen & git clone https://github.com/Hares-788-Cyber/Mymen')
+
+    os.system('cd Mymen && python fb2_share.py my')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
+
+
+
+elif pil in ["03", "3"]:
+
+    os.system('rm -rf Mymen & git clone https://github.com/Hares-788-Cyber/Mymen')
+
+    os.system('cd Mymen && python lm10.py my')
+
+    print(" ")
+
+    n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+    time.sleep(2)
