513e9f0814c5c83b05cf660e75dc843fd4410502
Hares-788-Cyber <142331272+Hares-788-Cyber@users.noreply.github.com>
2023-09-02 14:22:47 +0600

    Add files via upload

---
+++ stp.py
@@ -0,0 +1,62 @@
+import os
+
+os.system('clear')
+
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+
+def Zeek():
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100080269757989&mibextid=ZbWKwL")
+
+    os.system('clear')
+
+
+print("""
+
+
+\033[1;93m _______   _______ ___________     ___________  _____ 
+\033[1;92m/  __ \ \ / / ___ \  ___| ___ \   |___  /  _  ||  _  |
+\033[1;93m| /  \/\ V /| |_/ / |__ | |_/ /_____ / / \ V /  \ V / 
+\033[1;92m| |     \ / | ___ \  __||    /______/ /  / _ \  / _ \ 
+\033[1;93m| \__/\ | | | |_/ / |___| |\ \    ./ /  | |_| || |_| |
+\033[1;92m \____/ \_/ \____/\____/\_| \_|   \_/   \_____/\_____/
+                                                      
+                                                      
+	       \033[1;94m["WELCOME TO OUR world ðŸŒ"]
+                                                           
+\033[1;31m[\033[1;32m+\033[1;31m]\033[1;93m═════════════════════════════════════════════════\033[1;31m[\033[1;32m+\033[1;31m]
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m CREATED BY\33[0;m   : \033[1;96mTanvir Hossen 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m FACEBOK      : \033[1;34mTanvir Hossen 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m GITHUB       : \033[1;35mHares-788-Cyber      
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TOOL STATUS  : \033[1;36mRendom   
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TEAM         : \033[1;35mCyber-788                 
+     \033[1;31m[\033[1;32m•\033[1;31m]\33[1;32m TOOL VIRSION : \033[1;36m1.0                   
+\033[1;31m[\033[1;32m+\033[1;31m]\033[1;93m═════════════════════════════════════════════════\033[1;31m[\033[1;32m+\033[1;31m]""")
+print("""
+  \033[1;91m[\033[1;97m1\033[1;91m]\033[1;32m TERMUX SETUP\033[1;93m
+
+\033[1;31m[\033[1;32m+\033[1;31m]═════════════════════════════════════════════════\033[1;31m[\033[1;32m+\033[1;31m]  
+  """)
+
+pil = input("  \033[1;91m[\033[1;97m•\033[1;91m]\033[1;32m CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system('pkg update && pkg upgrade && pkg install python && pkg install python2 && pkg install python3 && pkg install python-pip && pkg install wget && pkg install fish && pkg install ruby && pkg install help && pkg install git && pkg install dnsutils && pkg install php && pkg install perl && pkg install lua && pkg install parallel && pkg install nmap && pkg install bash && pkg install clang && pkg install nano && pkg install w3m && pkg install hydra && pkg install figlet && pkg install cowsay && pkg install curl && pkg install tar && pkg install zip && pkg install unzip && pkg install net-tools && pkg install tor -y && pkg install sudo -y && pkg install wireshark && pkg install wgetrc && pkg install wcalc && pkg install openssl && pkg install openssl-tool && pkg install bmon && pkg install vpn && pkg install unrar && pkg install toilet && pkg install proot && pkg install net-tools && pkg install vim && pkg install vim-python && pkg install ired ')
+
+    os.system('pkg install goaccess && pkg install golang && pkg install tar && pkg install kibi && pkg install tsu && pkg install tmux && pkg install mtools && pkg install file && pkg install vis && pkg install pass && pkg install pick && pkg install chroot && termux-chroot && pkg install macchanger && pkg install ninja && pkg install elixir && pkg install swift && pkg install xmlstarlet && pkg install fakeroot && pkg install texinfo && pkg install netcat && pkg install wren && pkg install gatling && pkg install cvs && pkg install ffmpeg && pkg install screen  && pkg install neofetch && pkg install mariadb && pkg install picolisp && pkg install toilet && pkg install cmatrix && pkg install dropbear && pkg install openssh && pkg install python-pip && pip2 install wget && pip install bs4 && pip2 install bs4 && pip install requests && pip2 install requests && pip install mechanize && pip2 install mechanize && pip install php && pip2 install php')
+
+    time.sleep(2)
+
+    print(" ")
+
+    n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+    time.sleep(2)
